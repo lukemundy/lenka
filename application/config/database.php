c183ad469f11ae6c2e2490e9a79838102f3db1bc
Luke Mundy <lmundy@gmail.com>
2010-07-29 02:23:37 +0800

    Database configuration completed.

--- application/config/database.php
+++ application/config/database.php
@@ -38,12 +38,12 @@ $active_group = "default";
 $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
-$db['default']['username'] = "";
-$db['default']['password'] = "";
-$db['default']['database'] = "";
+$db['default']['username'] = "lenka_db";
+$db['default']['password'] = "%5bOyw@Wx^}^";
+$db['default']['database'] = "lenka";
 $db['default']['dbdriver'] = "mysql";
-$db['default']['dbprefix'] = "";
-$db['default']['pconnect'] = TRUE;
+$db['default']['dbprefix'] = "len_";
+$db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = "";
