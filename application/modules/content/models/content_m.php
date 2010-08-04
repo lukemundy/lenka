c5f747c95d88ef3316d26be9afc689e7928c6c6a
Luke Mundy <lmundy@gmail.com>
2010-08-05 01:03:25 +0800

    New function in content model - get_list(). Will return a list of all articles (to be used in the admin section).

--- application/modules/content/models/content_m.php
+++ application/modules/content/models/content_m.php
@@ -27,6 +27,23 @@ class Content_m extends Model
 		
 		return $content;
 	}
+	
+	/**
+	 * Returns a list of articles, minus their body.
+	 * @param int $n Number of articles to return
+	 * @param int $pg Page number for pagination
+	 * @return array
+	 */
+	function get_list($n, $pg)
+	{
+		$content = $this->db
+			->select('ID_CNT, stub, title, date, state')
+			->limit($n, (($pg-1) * $n))
+			->get('content')
+			->result_array();
+		
+		return $content;
+	}
 }
 
 // END - class Content_m
