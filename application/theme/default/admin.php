0aa9d35ad0e09f10b5add5c8ce293b7065796c36
Luke Mundy <lmundy@gmail.com>
2010-08-05 15:25:27 +0800

    Default admin theme created, two new icons added.

---
+++ application/theme/default/admin.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title><?= $page_title ?></title>
+	
+	<?= link_tag($theme_url . 'css/admin.css') ?>
+	<?= link_tag($theme_url . 'css/icons.css') ?>
+	
+	<script type="text/javascript" src="<?= $theme_url ?>js/jquery-1.4.2.min.js"></script>
+	
+	<script type="text/javascript">
+		$(document).ready(function () {
+			$('ul.dropdown li').hover(
+				function () { $(this).addClass('hover'); },
+				function () { $(this).removeClass('hover'); }
+			);
+			
+			$('ul.dropdown li.parent').hover(
+				function () {
+					$(this).children('ul:first').slideDown(100);
+				},
+				function () {
+					$(this).children('ul:first').fadeOut(100);
+				}
+			);
+		});
+	</script>
+</head>
+<body>
+	<div id="container">
+		<div id="admin-header">
+			<h1><?= $this->config->item('site_name') ?> // Administration</h1>
+		</div>
+		<div id="admin-menubar">
+			<ul class="dropdown">
+				<li class="parent">
+					<span>Site</span>
+					<ul>
+						<li>
+							<a href="#" class="icon-admincp">Control Panel</a>
+						</li>
+						<li>
+							<a href="#" class="icon-config">Configuration</a>
+						</li>
+						<li>
+							<a href="#" class="icon-userman">User Manager</a>
+						</li>
+						<li>
+							<a href="#" class="icon-logout">Logout</a>
+						</li>
+					</ul>
+				</li>
+				<li class="parent">
+					<span>Content</span>
+					<ul>
+						<li>
+							<a href="#" class="icon-newpage">New Article</a>
+						</li>
+						<li>
+							<a href="#" class="icon-articleman">Article Manager</a>
+						</li>
+						<li>
+							<a href="#" class="icon-encpage">Encrypted Articles</a>
+						</li>
+					</ul>
+				</li>
+				<li class="parent">
+					<span>Widgets</span>
+					<ul>
+						<li>
+							<a href="#" class="icon-newwidget">New Widget</a>
+						</li>
+						<li>
+							<a href="#" class="icon-widgetpos">Widget Positions</a>
+						</li>
+					</ul>
+				</li>
+				<li class="parent">
+					<span>Help</span>
+					<ul>
+						<li>
+							<a href="#" class="icon-help">Help Docs</a>
+						</li>
+						<li>
+							<a href="#" class="icon-sysinfo">System Information</a>
+						</li>
+						<li>
+							<a href="#" class="icon-authorlink">Author's Website</a>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		
+		<div id="module">
+			<?= $page_body ?>
+		</div>
+	</div>
+	
+	<div id="footer">
+		Powered by Lenka<br />Page generated in {elapsed_time} seconds
+	</div>
+</body>
+</html>
