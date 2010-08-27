a20df15a87b2f2c4806869e965e17700bb12d441
Luke Mundy <lmundy@gmail.com>
2010-08-28 02:17:21 +0800

    Logged in users will now be directed back to the control panel. Basic login/logout functionality implemented.

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -30,14 +30,50 @@ class Admin extends Backend_Controller
 		// Check for POST data
 		if ($_POST)
 		{
-			// ... do login
+			$this->session->set_userdata('logged_in', TRUE);
+			
+			// Send them back to where they came from, if possible
+			if ($this->session->flashdata('return_to'))
+			{
+				log_message('debug', 'Sending user back to: '. $this->session->flashdata('return_to'));
+				
+				redirect($this->session->flashdata('return_to'));
+			}
+			else
+			{
+				redirect(site_url('admin'));
+			}
 		}
 		else
 		{
+			// If user is already logged in, send them to the dashboard
+			if ($this->session->userdata('logged_in'))
+			{
+				redirect('admin');
+				
+				return;
+			}
+			
+			// Keep this until after the login has been processed
+			$this->session->keep_flashdata('return_to');
+			
 			$this->data['page_title'] = 'User Login';
 			
 			$this->template->render('admin/login', $this->data);
 		}
+	}
+	
+	/**
+	 * Logout
+	 *
+	 * Log the user out
+	 * @return void
+	 */
+	public function logout()
+	{
+		$this->session->unset_userdata('logged_in');
+		
+		redirect('admin/login');
 	}
 }
 
